eacf000adc882a8de8a4b08f6e12248de2b477f3
GahFerreira <49258183+GahFerreira@users.noreply.github.com>
2022-03-19 00:02:56 -0300

    Added state dropdown to route box

--- script.js
+++ script.js
@@ -8,34 +8,48 @@ function new_run()
 function new_route_box()
 {
   // Creating the outer gray box
-  new_box = document.createElement("div");
+  const new_box = document.createElement("div");
   new_box.className = "outer-gray-box containing-box";
   document.body.appendChild(new_box);
 
   // Creating the title that will hold the name of the route
-  route_name = document.createElement("p");
+  const route_name = document.createElement("p");
   route_name.className = "title-route";
   route_name.innerText = "Route 01";
 
   new_box.appendChild(route_name);
 
   // Creating the place where the pokemon image will be
-  img_place = document.createElement("div");
+  const img_place = document.createElement("div");
   img_place.className = "img-place";
-
   new_box.appendChild(img_place);
 
   // Creating the input field where the pokemon name will be typed
-  pokemon_field = document.createElement("input");
+  const pokemon_field = document.createElement("input");
   pokemon_field.className = "pokemon-input-place";
   pokemon_field.type = "text";
+  pokemon_field.setAttribute("maxlength", 12);
   // pokemon_field.autofocus = true;
+  new_box.appendChild(pokemon_field);
 
   // Creating the dropdown menu to control pokemon state
+  const state_dropdown = document.createElement("select");
+  state_dropdown.className = "state-dropdown";
 
-  new_box.append(pokemon_field);
+  const states = ["team", "boxed", "fainted", "escaped"];
+  states.className = "state-dropdown";
+
+  for (const state of states)
+  {
+    const new_option = document.createElement("option");
+    new_option.value = state;
+    new_option.text = state.charAt(0).toUpperCase() + state.slice(1);
+    state_dropdown.appendChild(new_option);
+  }
+
+  new_box.appendChild(state_dropdown);
 }
 
-new_run_button = document.getElementById("new-run-button");
+const new_run_button = document.getElementById("new-run-button");
 
 new_run_button.addEventListener("click", new_run);
