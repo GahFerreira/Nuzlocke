7c6ed87dc0ec76d7d443a190eb8363400e005de9
GahFerreira <49258183+GahFerreira@users.noreply.github.com>
2022-03-18 21:02:27 -0300

    Changed the structure of route_box

--- script.js
+++ script.js
@@ -13,30 +13,27 @@ function new_route_box()
   document.body.appendChild(new_box);
 
   // Creating the title that will hold the name of the route
-  title_route = document.createElement("div");
-  title_route.className = "title-route";
-
   route_name = document.createElement("p");
+  route_name.className = "title-route";
   route_name.innerText = "Route 01";
 
-  title_route.appendChild(route_name);
-  new_box.appendChild(title_route);
+  new_box.appendChild(route_name);
 
   // Creating the place where the pokemon image will be
   img_place = document.createElement("div");
   img_place.className = "img-place";
+
   new_box.appendChild(img_place);
 
   // Creating the input field where the pokemon name will be typed
-  pokemon_input_place = document.createElement("div");
-  pokemon_input_place.className = "pokemon-input-place";
-
   pokemon_field = document.createElement("input");
+  pokemon_field.className = "pokemon-input-place";
   pokemon_field.type = "text";
-  pokemon_field.autofocus = true;
+  // pokemon_field.autofocus = true;
 
-  pokemon_input_place.appendChild(pokemon_field);
-  new_box.append(pokemon_input_place);
+  // Creating the dropdown menu to control pokemon state
+
+  new_box.append(pokemon_field);
 }
 
 new_run_button = document.getElementById("new-run-button");
