05eb4057a09b718da70c4561a5aa1fbe3420dffa
GahFerreira <49258183+GahFerreira@users.noreply.github.com>
2022-05-19 21:02:17 -0300

    Standardize code formatting
    Code formatting was not good. Indentation was wrong, as spaces got mixed
    with tabs, and it was also not following any standard.
    
    This commit will standardize code formatting, following VSCode default
    formatter. There were a couple settings changed as described below.
    
    Notable standards that have been set:
    - <head> and <body> are not indented (default VSCode setting)
    - CSS Rules opens braces in new line
    - JavaScript opens block and function braces in new line
    - All indentations are tabs
    
    I am aware of JavaScript automatic semicolon placement, and how that
    interacts with 'return' when braces are put in a new line. However, at
    this point it feels easier to write cleaner code using braces in a new
    line. Either way, this rule may change in the future, if needed.

--- script.js
+++ script.js
@@ -4494,46 +4494,46 @@ const poke_data =
 
 function new_run()
 {
-  document.getElementById("menu").remove();
+	document.getElementById("menu").remove();
 
-  return new_route_box();
+	return new_route_box();
 }
 
 function new_route_box()
 {
-  // Creating the outer gray box
-  const new_box = document.createElement("div");
-  new_box.className = "outer-gray-box containing-box";
-  document.body.appendChild(new_box);
+	// Creating the outer gray box
+	const new_box = document.createElement("div");
+	new_box.className = "outer-gray-box containing-box";
+	document.body.appendChild(new_box);
 
-  // Creating the title that will hold the name of the route
-  const route_name = document.createElement("p");
-  route_name.className = "title-route";
-  route_name.innerText = "Route 01";
+	// Creating the title that will hold the name of the route
+	const route_name = document.createElement("p");
+	route_name.className = "title-route";
+	route_name.innerText = "Route 01";
 
-  new_box.appendChild(route_name);
+	new_box.appendChild(route_name);
 
-  // Creating the place where the pokemon image will be
-  const img_place = document.createElement("div");
-  img_place.className = "img-place";
-  new_box.appendChild(img_place);
+	// Creating the place where the pokemon image will be
+	const img_place = document.createElement("div");
+	img_place.className = "img-place";
+	new_box.appendChild(img_place);
 
-  // Creating the place where the pokemon selection will be
-  const pokemon_select_wrapper = document.createElement("div");
-  pokemon_select_wrapper.className = "pokemon-select-wrapper";
-  new_box.appendChild(pokemon_select_wrapper);
+	// Creating the place where the pokemon selection will be
+	const pokemon_select_wrapper = document.createElement("div");
+	pokemon_select_wrapper.className = "pokemon-select-wrapper";
+	new_box.appendChild(pokemon_select_wrapper);
 
-  // Creating the input field where the pokemon name will be typed
-  const pokemon_input_field = document.createElement("input");
-  pokemon_input_field.className = "pokemon-input-field";
-  pokemon_input_field.setAttribute("maxlength", 12);
+	// Creating the input field where the pokemon name will be typed
+	const pokemon_input_field = document.createElement("input");
+	pokemon_input_field.className = "pokemon-input-field";
+	pokemon_input_field.setAttribute("maxlength", 12);
 
 	// 'focus' & 'blur' events were removed because of a better implementation using CSS
 	// 'focus' means when the element gets in focus
-  // pokemon_input_field.addEventListener("focus", () =>
-  // {
-  //   pokemon_select_wrapper.classList.add("active");
-  // });
+	// pokemon_input_field.addEventListener("focus", () =>
+	// {
+	//   pokemon_select_wrapper.classList.add("active");
+	// });
 
 	// 'blur' means when the elements gets out of focus
 	// pokemon_input_field.addEventListener("blur", () =>
@@ -4542,30 +4542,30 @@ function new_route_box()
 	// 	// pokemon_select_wrapper.classList.remove("active");
 	// });
 
-  pokemon_select_wrapper.appendChild(pokemon_input_field);
+	pokemon_select_wrapper.appendChild(pokemon_input_field);
 
-  const pokemon_list = document.createElement("ul");
-  pokemon_list.className = "pokemon_list";
+	const pokemon_list = document.createElement("ul");
+	pokemon_list.className = "pokemon_list";
 
-  for (const { name, value, image } of poke_data.slice(0, 9))
-  {
-    const new_list_item = document.createElement("li");
+	for (const { name, value, image } of poke_data.slice(0, 9))
+	{
+		const new_list_item = document.createElement("li");
 		new_list_item.setAttribute("id", name);
 
-    const new_option = document.createElement("div");
-    new_option.className = "option";
+		const new_option = document.createElement("div");
+		new_option.className = "option";
 
 		// 'alt' was set to "" so non-visual browsers may omit it from rendering (it's decoration after all)
-    const new_icon = document.createElement("img");
+		const new_icon = document.createElement("img");
 		new_icon.src = image;
 		new_icon.setAttribute("alt", "");
-    new_option.appendChild(new_icon);
+		new_option.appendChild(new_icon);
 
-    const pokemon_name = document.createElement("p");
-    pokemon_name.innerText = name;
-    new_option.appendChild(pokemon_name);
+		const pokemon_name = document.createElement("p");
+		pokemon_name.innerText = name;
+		new_option.appendChild(pokemon_name);
 
-    new_list_item.appendChild(new_option);
+		new_list_item.appendChild(new_option);
 
 		new_list_item.addEventListener("click", () =>
 		{
@@ -4573,30 +4573,30 @@ function new_route_box()
 			// pokemon_select_wrapper.classList.remove("active");
 		});
 
-    pokemon_list.appendChild(new_list_item);
-  }
+		pokemon_list.appendChild(new_list_item);
+	}
 
-  pokemon_select_wrapper.appendChild(pokemon_list);
+	pokemon_select_wrapper.appendChild(pokemon_list);
 
-  // Creating the dropdown menu to control pokemon state
+	// Creating the dropdown menu to control pokemon state
 	const state_dropdown_wrapper = document.createElement("div");
 	state_dropdown_wrapper.className = "state-dropdown-wrapper";
 
-  const state_dropdown = document.createElement("select");
-  state_dropdown.className = "state-dropdown";
+	const state_dropdown = document.createElement("select");
+	state_dropdown.className = "state-dropdown";
 
-  const states = ["team", "boxed", "fainted", "escaped"];
+	const states = ["team", "boxed", "fainted", "escaped"];
 
-  for (const state of states)
-  {
-    const new_option = document.createElement("option");
-    new_option.value = state;
-    new_option.text = state.charAt(0).toUpperCase() + state.slice(1);
-    state_dropdown.appendChild(new_option);
-  }
+	for (const state of states)
+	{
+		const new_option = document.createElement("option");
+		new_option.value = state;
+		new_option.text = state.charAt(0).toUpperCase() + state.slice(1);
+		state_dropdown.appendChild(new_option);
+	}
 
 	state_dropdown_wrapper.appendChild(state_dropdown);
-  new_box.appendChild(state_dropdown_wrapper);
+	new_box.appendChild(state_dropdown_wrapper);
 }
 
 const new_run_button = document.getElementById("new-run-button");
